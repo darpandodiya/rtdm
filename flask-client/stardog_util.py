719036d793159e346818bbd706c20121b632bb5d
Vivek <vivek.7266@gmail.com>
2019-12-11 20:54:31 -0500

    changes for requires

--- flask-client/stardog_util.py
+++ flask-client/stardog_util.py
@@ -6,23 +6,42 @@ conn_details = {
     'password': 'admin'
 }
 
-def getTerms(inputTerm):
+PARENT_OF = ":parentOf"
+REQUIRES = ":requires"
+
+def getTerms(input_term):
     with stardog.Admin(**conn_details) as admin:
-        with stardog.Connection('rtdm', **conn_details) as conn:
-            results = conn.select("PREFIX : <http://resourcedescription.tut.fi/ontology/processTaxonomyModel#>\n" +
-                                  "select ?superclass where { \n" +
-                                  ":" + inputTerm + " rdfs:subClassOf* ?superclass\n" +
-                                  "}")
+        with stardog.Connection('capability', **conn_details) as conn:
+            # get all equivalent calsses query
+            # No. 1 : get all parents
+            query_for_parents = """
+                SELECT ?entity where {{
+                    ?src {0}* <http://stardog.com/{1}> .
+                    FILTER(?src != <http://stardog.com/ManufacturingCapabilities>)
+                    BIND(?src AS ?entity) .                   
+                }}
+                """.format(PARENT_OF, input_term)
+            result_for_parents = conn.select(query_for_parents, reasoning=True)
 
-            result_list = []
-            for result in results['results']['bindings']:
-                tempOut = result[next(iter(result))]['value']
-                tempOut = tempOut.split("#")[1]
+            # No. 2 : get all requires
+            query_for_requires = """
+                SELECT ?entity where {{
+                    <http://stardog.com/{1}> {0}* ?tgt .
+                    FILTER(?tgt != <http://stardog.com/ManufacturingCapabilities>)
+                    BIND(?tgt AS ?entity) .                   
+                }}
+                """.format(REQUIRES, input_term)
+            result_for_requires = conn.select(query_for_requires, reasoning=True)
 
-                if tempOut != 'ProcessTaxonomyElement':
-                    result_list.append(tempOut)
+            stardog_responses = [result_for_parents, result_for_requires]
+
+            result_list = set()
+            for response in stardog_responses:
+                for result in response['results']['bindings']:
+                    temp_out = result[next(iter(result))]['value']
+                    temp_out = temp_out.rsplit("/", 1)[-1]
+                    result_list.add(temp_out)
 
             return result_list
 
-
-# print(getTerms("Bagging"))
+print(getTerms("Casting"))
